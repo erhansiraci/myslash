31f82bb446fc862ed7ee4edcede7c4fadbd9a58e
erhansiraci <esiraci@gmail.com>
2017-04-05 22:15:23 +0300

    afaf

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from bottle import run, post,request
 def hello():
     command = request.forms.get('command')
     text = request.forms.get('text')
-    return 'Hello ' + var + text
+    return 'Hello ' + command + text
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
